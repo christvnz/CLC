a40159a913a0a301c3eec92595ece04d626ea7a6
Chris <chris.chen@tvnz.co.nz>
2024-07-14 22:34:36 +0900

    footer

--- src/components/templates/footer/index.tsx
+++ src/components/templates/footer/index.tsx
@@ -21,51 +21,52 @@ const TextP = styled.p`
 
 const Footer = () => {
   return (
-    <FooterContainer className="flex-column grid grid-cols-2 md:gap-12">
+    <FooterContainer className="flex-column">
       <WordLogo />
-
-      <div className="col-span-2 md:order-1 md:col-span-1">
-        <TextP className="mt-6">
-          chowluckclub.com is a digital platform designed to help you discover great restaurants,
-          products, and businesses in Auckland. It offers a comprehensive directory of dining
-          options, ranging from casual eateries to gourmet establishments, along with a variety of
-          other establishments. This platform also features articles from our writers showcasing top
-          spots, opinions, and in-depth restaurant features, making it a valuable resource for
-          discovering the best of Auckland's offerings.
-        </TextP>
-      </div>
-      <div className="md:order-0 col-span-2 mt-6 md:col-span-1">
-        <div className="">
-          <EmailSubscription />
+      <div className="grid grid-cols-2 md:gap-12">
+        <div className="col-span-2 md:order-1 md:col-span-1">
+          <TextP className="mt-6">
+            chowluckclub.com is a digital platform designed to help you discover great restaurants,
+            products, and businesses in Auckland. It offers a comprehensive directory of dining
+            options, ranging from casual eateries to gourmet establishments, along with a variety of
+            other establishments. This platform also features articles from our writers showcasing
+            top spots, opinions, and in-depth restaurant features, making it a valuable resource for
+            discovering the best of Auckland's offerings.
+          </TextP>
         </div>
-      </div>
+        <div className="md:order-0 col-span-2 mt-6 md:col-span-1">
+          <div className="">
+            <EmailSubscription />
+          </div>
+        </div>
 
-      <div className="order-2 col-span-2">
-        <FollowUs />
-      </div>
+        <div className="order-2 col-span-2">
+          <FollowUs />
+        </div>
 
-      <div className="order-2 col-span-2">
-        <div className="grid space-y-3">
-          <p>
-            <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/about">
-              About us
-            </Link>
-          </p>
-          <p>
-            <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/advertising">
-              Advertising & Partnership
-            </Link>
-          </p>
-          <p>
-            <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/policy">
-              Editorial Policy
-            </Link>
-          </p>
-          <p>
-            <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/contact">
-              Contact us
-            </Link>
-          </p>
+        <div className="order-2 col-span-2">
+          <div className="grid space-y-3">
+            <p>
+              <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/about">
+                About us
+              </Link>
+            </p>
+            <p>
+              <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/advertising">
+                Advertising & Partnership
+              </Link>
+            </p>
+            <p>
+              <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/policy">
+                Editorial Policy
+              </Link>
+            </p>
+            <p>
+              <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/contact">
+                Contact us
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </FooterContainer>
