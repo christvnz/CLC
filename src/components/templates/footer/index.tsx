1ee81ee985d282e6d9bea6358d0f52459245571c
Chris <chris.chen@tvnz.co.nz>
2024-08-20 23:47:51 +0800

    honda logo

--- src/components/templates/footer/index.tsx
+++ src/components/templates/footer/index.tsx
@@ -1,8 +1,11 @@
 import WordLogo from '../header/wordLogo.svg';
 import EmailSubscription from '@src/components/features/emailSubscription';
 import FollowUs from './FollowUs';
+import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
+import honda from './honda.png';
+import HNZ from './HNZ_logo.png';
 
 const FooterColor = styled.footer`
   background-color: #e08c54;
@@ -15,6 +18,15 @@ const FooterContainer = styled.div`
 
   @media (min-width: 768px) {
     padding: 0 87px 37px;
+  }
+`;
+
+const StyledTitle = styled.h2`
+  color: white;
+  font-weight: 400;
+  margin-bottom: 16px;
+  @media (min-width: 1024px) {
+    font-size: 42px;
   }
 `;
 
@@ -40,8 +52,8 @@ const Footer = () => {
         <div className="grid grid-cols-3 md:mt-6 md:gap-10 lg:gap-16">
           <div className="col-span-3 md:order-1 md:col-span-2">
             <TextP className="mb-6 mt-6 md:mb-0 md:mt-0">
-              <span className="font-bold">chowluckclub.com</span>{' '}
-              {` is a digital platform designed to help you discover great restaurants,
+              <span className="font-bold">chowluckclub.com</span>
+              {`is a digital platform designed to help you discover great restaurants,
             products, and businesses in Auckland. It offers a comprehensive directory of dining
             options, ranging from casual eateries to gourmet establishments, along with a variety of
             other establishments. This platform also features articles from our writers showcasing
@@ -54,14 +66,12 @@ const Footer = () => {
               <EmailSubscription />
             </div>
           </div>
-
-          <div className="order-2 col-span-2 mt-8 mb-8 md:mt-0 md:mb-0">
+          <div className="order-2 col-span-2 mt-8 mb-8 md:order-1 md:col-span-2 md:mt-0 md:mb-0">
             <Title>Follow us</Title>
 
             <FollowUs />
           </div>
-
-          <div className="order-2 col-span-2 md:col-span-2">
+          <div className="order-2 col-span-2 md:col-span-3">
             <div className="grid space-y-3">
               <TextP>
                 <Link className=" hover:text-gray-200 inline-flex gap-x-2" href="/about">
@@ -85,7 +95,16 @@ const Footer = () => {
               </TextP>
             </div>
           </div>
-          <div className="order-2 col-span-3 mt-3 flex justify-center md:col-span-1 md:items-end md:justify-end">
+          <div className="order-2 col-span-3 mt-5 flex  items-center md:order-2 md:col-span-1 md:justify-start">
+            <StyledTitle>PARTNER WITH</StyledTitle>
+          </div>
+          <div className="order-2 col-span-3 mt-3 flex justify-center md:order-2 md:col-span-1 ">
+            <Image src={HNZ} width={200} height={200} alt="Hospitality New Zealand" />
+          </div>
+          <div className="order-2 col-span-3 mt-3 flex items-center justify-center md:order-2 md:col-span-1">
+            <Image src={honda} width={200} height={35} alt="Honda" />
+          </div>
+          <div className="order-2 col-span-3 mt-8 flex md:col-span-3 md:items-end md:justify-end">
             <span className=" hover:text-gray-200 inline-flex gap-x-2">{`©Lincoln's Table 2024`}</span>
             <Link className=" hover:text-gray-200 ml-2 inline-flex gap-x-2" href="/terms">
               T&Cs / Privacy Policy
