c17d375fa823a5d24d90f64b2e8758405bcf2d7a
Sanket Jariwala <sanketjariwala45@gmail.com>
2025-01-13 11:06:56 +0530

    Remove comment

--- src/components/features/article/ArticleSocialShare.tsx
+++ src/components/features/article/ArticleSocialShare.tsx
@@ -28,7 +28,6 @@ interface Props {
   slug: string;
 }
 
-// Add Social Share Buttons
 const ArticleSocialShare = (props: Props) => {
   const location = `${process.env.NEXT_PUBLIC_BASE_URL}${props.slug}`;
   const title = props.title || '';
