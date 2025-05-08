32ac50c06896c5ccdf00cf5a2ece132ab20e7968
Sanket Jariwala <sanketjariwala45@gmail.com>
2025-05-08 19:09:33 +0530

    Added Android App link in the footer

--- src/components/templates/footer/v2_footer.tsx
+++ src/components/templates/footer/v2_footer.tsx
@@ -105,7 +105,10 @@ const V2Footer = () => {
                         <div className='flex justify-center'>
                             <div className="inline-flex items-center gap-3 rounded-xl px-4 py-2">
                                 <a href="https://apps.apple.com/nz/app/chowluckcard/id6744236704">
-                                    <Image src="/appstore.svg" width={200} height={50} alt="appstore" />
+                                    <Image src="/appstore.svg" width={200} height={50} alt="appstore" className='object-contain'/>
+                                </a>
+                                <a href="https://play.google.com/store/apps/details?id=com.chowluckclub">
+                                    <Image src="/googleplay.webp" width={200} height={50} alt="googleplay" className='object-contain'/>
                                 </a>
                             </div>
                         </div>
